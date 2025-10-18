34734a68e56b5c60eb1e29d39b9ae1dda47f7496
KNS121 <komaroff.ns@gmail.com>
2025-10-18 21:55:33 +0300

    msg to packages

--- Lab1/StopAndWait.py
+++ Lab1/StopAndWait.py
@@ -1,7 +1,43 @@
 import hashlib
 
-data1 = b"hello world"
-data2 = b"Hello world"
+data = "Hello world11"
+package_data_size = 2
 
-print(hashlib.sha256(data1).hexdigest())
-print(hashlib.sha256(data2).hexdigest())
+def msg_to_packeges(data):
+
+    packages = []
+
+    for i in range(0,len(data), package_data_size):
+        #print(data[i:i+package_data_size])
+        package = {
+            "seq_num": 0,
+            "hash_sum": hashlib.sha256(b"{data[i:i+package_data_size]}").hexdigest(),
+            "data": data[i:i+package_data_size]
+        }
+
+        packages.append(package)
+
+    return packages
+
+
+print(msg_to_packeges(data))
+
+
+
+
+
+#packet1 = {
+##    "seq_number": 1,
+#    "hash_sum": hashlib.sha256(b"{data1}").hexdigest(),
+#    "data": data1
+#}
+
+#packet2 = {
+#    "seq_number": 1,
+#    "hash_sum": hashlib.sha256(b"{data2}").hexdigest(),
+#    "data": data1
+#}
+
+
+
+#print([packet1, packet2])
