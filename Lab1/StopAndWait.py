2c48498e6fcfa348eb586ab8a46df4b4d7248f80
KNS121 <komaroff.ns@gmail.com>
2025-10-18 23:43:09 +0300

    init timeer

--- Lab1/StopAndWait.py
+++ Lab1/StopAndWait.py
@@ -1,26 +1,88 @@
 import hashlib
+import time
 
-data = "Hello world11"
+data = "Hello worldaa"
 package_data_size = 2
+
+
+def CalculateHashSum(data):
+    return hashlib.sha256(data.encode()).hexdigest()
+
 
 def msg_to_packeges(data):
 
     packages = []
 
     for i in range(0,len(data), package_data_size):
-        #print(data[i:i+package_data_size])
+
+        package_nubmer = (i//package_data_size)%2
+
         package = {
-            "seq_num": 0,
-            "hash_sum": hashlib.sha256(b"{data[i:i+package_data_size]}").hexdigest(),
+            "seq_num": package_nubmer,
+            "hash_sum": CalculateHashSum(data[i:i+package_data_size]),
             "data": data[i:i+package_data_size]
         }
 
         packages.append(package)
 
+
     return packages
 
 
-print(msg_to_packeges(data))
+#print(msg_to_packeges(data))
+
+#class Sender:
+
+#    def __init__(self):
+#        
+#        self.check_ack = False
+#        self.check_time = True
+
+
+#    def send_package(package):
+        
+#        while 
+
+
+
+def Sender(data):
+    packages = msg_to_packeges(data)
+
+    #package_index = 0
+
+    for i in range(len(packages)):
+
+        package = packages[i]
+        check_package = False
+
+        start_time = time.time()
+
+        print(f"start_time {start_time}")
+
+        timedelta = 2
+
+        while (timedelta > 0.5):
+            print(package)
+            Reciever(package)            
+            check_package = Reciever(package)
+            timedelta = time.time() - start_time
+
+
+
+def Reciever(package):
+    check_hash_sum = False
+
+    if CalculateHashSum(package["data"]) == package["hash_sum"] :
+        check_hash_sum = True
+
+    time.sleep(0.2)
+
+    print(check_hash_sum)
+
+    return check_hash_sum
+
+
+Sender(data)
 
 
 
